2ad815cbd9357dd6e0c59ea80af8869c2470fe9a
samson <ksamy90@aol.com>
2022-11-24 18:56:49 +0300

    changes

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -5,7 +5,7 @@ import ButtonGroup from '../elements/ButtonGroup';
 import Button from '../elements/Button';
 import Image from '../elements/Image';
 import Modal from '../elements/Modal';
-
+import '../../styles/mine.css'
 const propTypes = {
   ...SectionProps.types
 }
@@ -60,22 +60,40 @@ const Hero = ({
       <div className="container-sm">
         <div className={innerClasses}>
 
-        <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="800">
-              <Image
-                className="has-shadow" id='diaper'
-                src={require('./../../assets/images/diaper2.jpg')}
-                alt="Hero" />
+        <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="800" id='heroic'>
+        <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200" id='hero-title'>
+              A+ Diaper <span className="text-color-primary" id='manage'>Management</span>
+            </h1>
+            <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400" id='p'>
+                Our solution aims at creating a link between the <br /> users and the recyclers for effective diaper disposal
+                </p>
+              
+              
+                
           </div>
+
+          
+          <div className="middle-hero">
+                <div className="waste-services">
+                    <div className='waste-head'>
+                        <h3 id='find'>Find sustainable waste services</h3>
+                    </div>
+                    <div className="waste-input">
+                        <input type="text" placeholder='Kisumu' id='service-input' />
+                        <select name="" id="" className='select-service'>
+                            <option value="">For Home</option>
+                            <option value="">For Business</option>
+                        </select>
+                        <a href="#" id='show-services'> Show services</a>
+                    </div>
+                </div>
+            </div>
 
 
           <div className="hero-content">
-            <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
-            </h1>
-            <div className="container-xs">
-              <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
-                </p>
+            
+            {/* <div className="container-xs">
+              
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
                   <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
@@ -86,15 +104,8 @@ const Hero = ({
                     </Button>
                 </ButtonGroup>
               </div>
-            </div>
+            </div> */}
           </div>
-          
-          <Modal
-            id="video-modal"
-            show={videoModalActive}
-            handleClose={closeModal}
-            video="https://player.vimeo.com/video/174002812"
-            videoTag="iframe" />
         </div>
       </div>
     </section>
