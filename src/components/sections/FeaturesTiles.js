e4d35753d44d7f265e24cf02ca2c53b18f993c5a
Sephens <adesephens@gmail.com>
2022-11-24 23:02:34 +0300

    sleepy

--- src/components/sections/FeaturesTiles.js
+++ src/components/sections/FeaturesTiles.js
@@ -3,6 +3,7 @@ import classNames from 'classnames';
 import { SectionTilesProps } from '../../utils/SectionProps';
 import SectionHeader from './partials/SectionHeader';
 import Image from '../elements/Image';
+import sample from '../../assets/images/bin3.jpg'
 
 const propTypes = {
   ...SectionTilesProps.types
@@ -60,21 +61,20 @@ const FeaturesTiles = ({
 
             <div className="tiles-item reveal-from-bottom">
               <div className="tiles-item-inner">
-                <div className="features-tiles-item-header">
+                <div className="features-tiles-item-header bin-divs">
                   <div className="features-tiles-item-image mb-16">
-                    <Image
+                    <Image className='bin-img'
                       src={require('./../../assets/images/bin1.jpg')}
                       alt="Features tile icon 01"
-                      width={64}
-                      height={64} />
+                     />
                   </div>
                 </div>
                 <div className="features-tiles-item-content">
-                  <h4 className="mt-0 mb-8">
+                  <h4 className="service-title">
                     Residential Pickup
                     </h4>
-                  <p className="m-0 text-sm">
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.
+                  <p className="service-desc">
+                    Make your home clean by choosing the best of waste management services. Let us keep the environment clean by kicking diapers away fro home.
                     </p>
                 </div>
               </div>
@@ -82,25 +82,89 @@ const FeaturesTiles = ({
 
             <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
               <div className="tiles-item-inner">
-                <div className="features-tiles-item-header">
+                <div className="features-tiles-item-header bin-divs">
                   <div className="features-tiles-item-image mb-16">
-                    <Image
-                      src={require('./../../assets/images/bin2.jpg')}
+                    <Image className='bin-img'
+                      src={require('./../../assets/images/bin4.jpg')}
                       alt="Features tile icon 02"
-                      width={64}
-                      height={64} />
+                    />
                   </div>
                 </div>
                 <div className="features-tiles-item-content">
-                  <h4 className="mt-0 mb-8">
+                  <h4 className="service-title">
                     Business Pickup
                     </h4>
-                  <p className="m-0 text-sm">
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.
+                  <p className="service-desc">
+                    Take care of the business we will take care of the waste for you. Choose ADM for reliable,responsible waste pickup.
                     </p>
                 </div>
               </div>
             </div>
+
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header bin-divs">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image className='bin-img'
+                      src={require('./../../assets/images/bin4.jpg')}
+                      alt="Features tile icon 02"
+                    />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="service-title">
+                    Roll-Off Dumpsters
+                    </h4>
+                  <p className="service-desc">
+                    Take care of the business we will take care of the waste for you. Choose ADM for reliable,responsible waste pickup.
+                    </p>
+                </div>
+              </div>
+            </div>
+
+
+          <div className='more'>
+            <h1 id='more-title'>Do More with ADM </h1>
+            <a href="#" id='more-btn'> Sign Up for My ADM</a>
+          </div>
+
+          <div className='more-small'>
+
+            <div className='see-schedule'>
+              <img src={sample} className='imago'/>
+              <h4>Hello</h4>
+            </div>
+            <div className='pay-bill'>
+            <img src={sample} className='imago'/>
+              <h4>Hello</h4>
+            </div>
+            <div className='see-schedule'>
+            <img src={sample} className='imago'/>
+              <h4>Hello</h4>
+            </div>
+            <div className='pay-bill'>
+            <img src={sample} className='imago'/>
+              <h4>Hello</h4>
+            </div>
+
+          </div>
+
+          <div className='learn'>
+            <img src={sample} id='learn-img'/>
+            <div>
+              <h3>Learn more on Environmental Hygiene</h3>
+              <p> lorem </p>
+              <a href="#" id='more-btn'> Sign Up for My ADM</a>
+            </div>
+
+          </div>
+
+          <div className='assistance'>
+            <h3>Need Assistance?</h3>
+            <p>Our team is readily available to offer an hand in case of problem</p>
+            <a href="#" id='more-btn'> View support center</a>
+          </div>
 
 
           </div>
